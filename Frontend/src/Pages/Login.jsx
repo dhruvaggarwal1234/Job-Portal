858706df49081a0dbedc5443d6038b5b5b9acda0
dhruvaggarwal1234 <dhruvaggarwal755@gmail.com>
2026-01-14 22:07:29 +0530

    Login_Update

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -1,4 +1,4 @@
-
+import Bg_Img from "../assets/BG_login.jpg"
 
 export default function Login() {
 
@@ -6,28 +6,39 @@ export default function Login() {
 
         <div className="bg-blue-400 w-full h-screen flex justify-center items-center">
 
-            <div className="bg-white w-[80%] h-[80%] rounded-3xl shadow-md  ring-4 ring-blue-800 flex  justify-evenly overflow-hidden 
+            <div className="bg-white w-[80%] h-[80%] rounded-3xl flex  justify-evenly overflow-hidden 
             
-            ">  
+            ">
 
                 {/* Right Side */}
-                <div className="w-full h-full ">Right</div>
+
+
+                <div className="flex w-full h-full bg-slate-800"><div className="flex w-full h-full  opacity-40">
+                        <img src={Bg_Img} alt="" />
+
+                        <div>JobPortal</div>
+                        
+                     
+
+                </div>
+                </div>
+
 
 
                 {/*Left Side  */}
-                <div className="w-full h-full flex items-baseline flex-col">
+                <div className="w-full h-full flex items-baseline bg- flex-col bg-slate-200">
                     {/* This is heading */}
                     <h3 className="m-[10%] ml-10  mb-5 text-2xl font-bold">JobPortal</h3>
-                  
+
                     <div className="text-4xl font-bold" >Welcome Back!</div>
                     <p className="mt-3 font-light">Please enter your details to Sign in.</p>
-                    
-                    <form className="flex flex-col m-10  w-full gap-3">
+
+                    <form className="flex flex-col m-10  w-full gap-3 ">
                         <label htmlFor="email">Email Address</label>
-                        <input type="email" name="email" id="email" className="w-[80%] h-10.5 border-2" required />
+                        <input type="email" name="email" id="email" className="w-[80%] h-10.5 border-2 rounded-full" required autoSave="false" defaultValue={false} />
 
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="password" className="w-[80%] h-10.5 border-2" required minLength={8}/>
+                        <input type="password" name="password" id="password" className="w-[80%] rounded-full h-10.5 border-2" required minLength={8} />
 
                     </form>
                 </div>
